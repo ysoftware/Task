a98175c1a7ea55db6f043d7dd243cb7d0bb14210
Iaroslav Erokhin <ysoftware@yandex.ru>
2026-03-25 18:06:27 +0100

    update output format

--- main.c
+++ main.c
@@ -234,10 +234,11 @@ skip:
         int print_length = task->title.count;
         if (task_title_fixed_length > 0) print_length = MIN(task_title_fixed_length, print_length);
 
-        printf("tasks/" SV_Fmt "/task.md:1:1 ", SV_Arg(task->file_path));
-        if (!only_closed) printf("|%2d| ", task->priority);
-        printf("%-*.*s", task_title_fixed_length, print_length, (task->title).data);
-        printf(" | " SV_Fmt "\n", SV_Arg(task->tags));
+        printf("tasks/" SV_Fmt "/task.md:1:1", SV_Arg(task->file_path));
+        printf(" %-*.*s", task_title_fixed_length, print_length, (task->title).data);
+        printf(" |");
+        if (!only_closed) printf("%2d|", task->priority);
+        printf(" " SV_Fmt "\n", SV_Arg(task->tags));
     }
 
     da_free(children);
